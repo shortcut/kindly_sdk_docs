1311ea47cf3426dfd3d0f717e7d13716820ce14b
Alex Karpenko <oleksandr.karpenko@shortcut.no>
2022-09-27 19:47:28 +0200

    Added documentation

---
+++ scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies='{":kindlysdk:dokkaHtml/androidTestRelease":[],":kindlysdk:dokkaHtml/debug":[],":kindlysdk:dokkaHtml/main":[],":kindlysdk:dokkaHtml/release":[],":kindlysdk:dokkaHtml/testFixtures":[],":kindlysdk:dokkaHtml/testFixturesDebug":[],":kindlysdk:dokkaHtml/testFixturesRelease":[]}'
